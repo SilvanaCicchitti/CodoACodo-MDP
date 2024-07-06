0eb9b7c759ee0aac391c1b2cd9b84cb9eb031860
Silvana <cicchittisilvana1986@gmail.com>
2024-07-06 17:24:16 -0300

    getProductoById funcionando ("/productos/id")

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -8,6 +8,20 @@ const getAllProducts = (req,res) =>{
     })
 }
 
+const getProductsById =(req,res) => {
+    const {id_product} =req.params;
+    const sql ='SELECT * FROM products WHERE id_product = ?';
+    db.query(sql, [id_product], (err, results) =>{
+        if (err){
+            console.error('Error al obtener producto:', err);
+            res.status(500).json({error:'Error al obtener producto'});
+            return;
+        }
+        res.json(results);
+    })
+}
+
 module.exports ={
-    getAllProducts
+    getAllProducts,
+    getProductsById
 }
--- routes/productosRoutes.js
+++ routes/productosRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const productosController =require ('../controllers/productosController')
 
 router.get('/', productosController.getAllProducts)
-//router.get('/:id', productosController.getProductsById)
+router.get('/:id_product', productosController.getProductsById)
 //router.post('/', productosController.createProduct)
 //router.put('/:id', productosController.updateProduct)
 //router.delete('/:id', productosController.deleteProduct)
